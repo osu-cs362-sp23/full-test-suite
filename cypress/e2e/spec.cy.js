416ab910619cac64e0fa63dcfddacbeb0012e191
oliverflood <oliveroldrich@gmail.com>
2023-06-12 16:12:29 -0700

    Made localhost:8081 the default url for Cypress testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+
+it("generates a chart correctly", function () {
+    cy.visit("/line.html")
+
+    
 })
