de0a31c8d3a209c534cfede8035f94ed14c49c51
oliverflood <oliveroldrich@gmail.com>
2023-06-12 17:50:06 -0700

    Added test for chart saving

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -18,46 +18,27 @@ it("generates a chart correctly", function () {
     cy.findByRole('img').should('exist');
 })
 
-// it("maintains chart data across pages", function () {
-//     cy.visit("/line.html")
 
-//     // type in data
-//     cy.contains("Chart title")
-//         .type("Cats vs. Dogs")
-//     cy.contains("X label")
-//         .type("Cats")
-//     cy.contains("Y label")
-//         .type("Dogs")
-//     cy.findByLabelText("X")
-//         .type("1")
-//     cy.findByLabelText("Y")
-//         .type("2")
+it("saves a chart to the gallery", function () {
+    cy.visit("/line.html")
 
-//     // check scatterplot page
-//     cy.findByRole("link", { name: "Scatter" }).click()
-//     cy.findByRole('textbox', { name: "Chart title" }).should('have.value', "Cats vs. Dogs");
-//     cy.findByRole('textbox', { name: "X label" }).should('have.value', "Cats");
-//     cy.findByRole('textbox', { name: "Y label" }).should('have.value', "Dogs");
-//     // unable to find text fields for "X" and "Y", needs to be added
-//     // cy.findByLabelText("X").should('have.value', "1");
-//     // cy.findByLabelText("Y").should('have.value', "2");
+    // type in data
+    cy.contains("Chart title")
+        .type("Cats vs. Dogs")
+    cy.contains("X label")
+        .type("Cats")
+    cy.contains("Y label")
+        .type("Dogs")
+    cy.findByLabelText("X")
+        .type("1")
+    cy.findByLabelText("Y")
+        .type("2")
 
-//     // check bar graph page
-//     cy.findByRole("link", { name: "Bar" }).click()
-//     cy.findByRole('textbox', { name: "Chart title" }).should('have.value', "Cats vs. Dogs");
-//     cy.findByRole('textbox', { name: "X label" }).should('have.value', "Cats");
-//     cy.findByRole('textbox', { name: "Y label" }).should('have.value', "Dogs");
-//     // unable to find text fields for "X" and "Y", needs to be added
-//     // cy.findByLabelText("X").should('have.value', "1");
-//     // cy.findByLabelText("Y").should('have.value', "2");
+    // generate and save chart
+    cy.findByRole("button", { name: "Generate chart" }).click()
+    cy.findByRole("button", { name: "Save chart" }).click()
 
-//     // check line graph page
-//     cy.findByRole("link", { name: "Line" }).click()
-//     cy.findByRole('textbox', { name: "Chart title" }).should('have.value', "Cats vs. Dogs");
-//     cy.findByRole('textbox', { name: "X label" }).should('have.value', "Cats");
-//     cy.findByRole('textbox', { name: "Y label" }).should('have.value', "Dogs");
-//     // unable to find text fields for "X" and "Y", needs to be added
-//     // cy.findByLabelText("X").should('have.value', "1");
-//     // cy.findByLabelText("Y").should('have.value', "2");
-
-// })
+    // check that the chart title is in gallery  
+    cy.findByRole("link", { name: "Gallery" }).click()
+    cy.findByText("Cats vs. Dogs").should("exist")
+})
