b297eb22314be7fa6e9777e0ab1a91a432f79eeb
oliverflood <oliveroldrich@gmail.com>
2023-06-12 18:09:31 -0700

    Added a test for re-opening saved charts

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -9,9 +9,9 @@ it("generates a chart correctly", function () {
     cy.contains("Y label")
         .type("Dogs")
     cy.findByLabelText("X")
-        .type("1")
+        .type("123")
     cy.findByLabelText("Y")
-        .type("2")
+        .type("456")
 
     // generate chart and check it's existence
     cy.findByRole("button", { name: "Generate chart" }).click()
@@ -64,4 +64,36 @@ it("saves a chart to the gallery", function () {
     // check that the chart title is in gallery  
     cy.findByRole("link", { name: "Gallery" }).click()
     cy.findByText("Cats vs. Dogs").should("exist")
+})
+
+it("re-opens a saved chart", function () {
+    cy.visit("/line.html")
+
+    // type in data
+    cy.contains("Chart title")
+        .type("Cats vs. Dogs")
+    cy.contains("X label")
+        .type("Cats")
+    cy.contains("Y label")
+        .type("Dogs")
+    cy.findByLabelText("X")
+        .type("123")
+    cy.findByLabelText("Y")
+        .type("456")
+
+    // generate and save chart
+    cy.findByRole("button", { name: "Generate chart" }).click()
+    cy.findByRole("button", { name: "Save chart" }).click()
+
+    // go to gallery then click on chart
+    cy.findByRole("link", { name: "Gallery" }).click()
+    cy.findByText("Cats vs. Dogs").click()
+
+    // test that the chart is saved
+    cy.findByRole('img').should('exist');
+    cy.findByRole('textbox', { name: "Chart title" }).should('have.value', "Cats vs. Dogs")
+    cy.findByRole('textbox', { name: "X label" }).should('have.value', "Cats")
+    cy.findByRole('textbox', { name: "Y label" }).should('have.value', "Dogs")
+    cy.findByDisplayValue("123").should("exist")
+    cy.findByDisplayValue("456").should("exist")  
 })
