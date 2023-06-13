ffe6cbfdce3855bdb63704ca96819d3146182523
oliverflood <oliveroldrich@gmail.com>
2023-06-12 18:05:32 -0700

    Added test to esnure data is maintained across pages

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -18,6 +18,29 @@ it("generates a chart correctly", function () {
     cy.findByRole('img').should('exist');
 })
 
+it("maintains chart data across pages", function () {
+    cy.visit("/line.html")
+
+    // type in data
+    cy.contains("Chart title")
+        .type("Cats vs. Dogs")
+    cy.contains("X label")
+        .type("Cats")
+    cy.contains("Y label")
+        .type("Dogs")
+    cy.findByLabelText("X")
+        .type("123")
+    cy.findByLabelText("Y")
+        .type("456")
+
+    // check scatterplot page
+    cy.findByRole("link", { name: "Scatter" }).click()
+    cy.findByRole('textbox', { name: "Chart title" }).should('have.value', "Cats vs. Dogs")
+    cy.findByRole('textbox', { name: "X label" }).should('have.value', "Cats")
+    cy.findByRole('textbox', { name: "Y label" }).should('have.value', "Dogs")
+    cy.findByDisplayValue("123").should("exist")
+    cy.findByDisplayValue("456").should("exist")
+})
 
 it("saves a chart to the gallery", function () {
     cy.visit("/line.html")
