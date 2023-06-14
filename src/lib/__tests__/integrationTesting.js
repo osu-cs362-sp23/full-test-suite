947947099283efccc64a13ce26c8b42aa76b5a1d
oliverflood <oliveroldrich@gmail.com>
2023-06-14 15:14:32 -0700

    Added some tests

---
+++ src/lib/__tests__/integrationTesting.js
@@ -0,0 +1,40 @@
+/**
+ * @jest-environment jsdom
+ */
+
+require("@testing-library/jest-dom/extend-expect")
+const domTesting = require("@testing-library/dom")
+const userEvent = require("@testing-library/user-event").default
+
+const fs = require("fs")
+
+function initDomFromFiles(htmlPath, jsPath) {
+    const html = fs.readFileSync(htmlPath, 'utf8')
+    document.open()
+    document.write(html)
+    document.close()
+    jest.isolateModules(function () {
+        require(jsPath)
+    })
+}
+
+test("values are correctly added in chart builder", async function () {
+    initDomFromFiles(
+        __dirname + "/../../bar/bar.html",
+        __dirname + "/../../bar/bar.js"
+    )
+    XInput = domTesting.getAllByLabelText(document, "X")
+    YInput = domTesting.getAllByLabelText(document, "Y")
+    const addValuesButton = domTesting.getByText(document, "+")
+
+    const user = userEvent.setup()
+    await user.type(XInput[0], "123")
+    await user.type(YInput[0], "a")
+    await user.click(addValuesButton)
+
+    var a = 123
+    var b = 456
+
+    expect(XInput[0]).toHaveValue(`${a}`)
+    expect(YInput[0]).toHaveValue("a")
+})
